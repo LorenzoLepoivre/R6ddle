8241506a0b3532e888c815a26a05778df0ffc16b
SupPepper <99681586+LorenzoLepoivre@users.noreply.github.com>
2024-07-21 14:41:44 +0200

    Update firebase.d.ts

--- src/models/firebase.d.ts
+++ src/models/firebase.d.ts
@@ -5,5 +5,8 @@ declare module '../models/firebase' {
     export function addCharacter(language: string, character: any, id: string): Promise<void>;
     export function addCharacters(language: string, allCharacters: any[]): Promise<void>;
     export function selectRandomCharacter(language: string): void;
+    export class Character {
+      constructor(id: string, name: string, camp: string, type: string, hp: number, speed: number, difficulty: string, year: string, birthplace: string, image: string, today: boolean);
+    }
   }
   
